cc9a4d3a07be3965016151de2b19fb1f883f8744
Maria Szegedy <mszegedy2@gmail.com>
2015-02-18 20:31:10 -0500

    Added license comments to main.cc

--- main.cc
+++ main.cc
@@ -1,4 +1,19 @@
+// main.cc
+// mszegedy, 2015
+
+/* This work is licensed under the Creative Commons Attribution 4.0
+ * International License. To view a copy of this license, visit
+ * http://creativecommons.org/licenses/by/4.0/ or send a letter to Creative
+ * Commons, PO Box 1866, Mountain View, CA 94042, USA.
+ *
+ * Main file of Garden.
+ */
+
+
+// SFML libraries
 #include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
 
 int main() {
   sf::RenderWindow window(sf::VideoMode(1024, 682), "Garden");
